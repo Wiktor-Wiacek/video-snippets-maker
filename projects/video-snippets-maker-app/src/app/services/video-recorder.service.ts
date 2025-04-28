1679de5385914f29dee76e4a2fe456872f015630
Wiktor Wiącek <wiktorw@backbase.com>
2025-04-28 21:45:16 +0200

    chore(recorder-service): RecorderService refactor

--- projects/video-snippets-maker-app/src/app/services/video-recorder.service.ts
+++ projects/video-snippets-maker-app/src/app/services/video-recorder.service.ts
@@ -12,7 +12,11 @@ export class VideoRecorderService {
   private mediaStreamProvider = inject(MediaStreamProvider);
 
   #stream: MediaStream | null = null;
-  #config: { duration: number } = { duration: 10000 };
+  //TODO: Get config from config service
+  #config: { duration: number; isMuted: boolean } = {
+    duration: 10000,
+    isMuted: true,
+  };
   #mediaRecorder: MediaRecorder | null = null;
 
   async startCamera(resolution?: string | undefined): Promise<MediaStream> {
@@ -30,7 +34,7 @@ export class VideoRecorderService {
 
   startRecording() {
     if (this.#stream) {
-      this.#handleMediaRecorder(this.#stream);
+      this.handleMediaRecorder(this.#stream);
     }
   }
 
@@ -38,75 +42,94 @@ export class VideoRecorderService {
     this.#mediaRecorder?.stop();
   }
 
-  #handleMediaRecorder(stream: MediaStream) {
+  private handleMediaRecorder(stream: MediaStream) {
     const chunks: Blob[] = [];
     const mediaRecorder = new MediaRecorder(stream);
 
     this.#mediaRecorder = mediaRecorder;
 
-    // Create video element to capture thumbnail
-    const videoElement = document.createElement('video');
-    videoElement.srcObject = stream;
-    videoElement.muted = true;
-    videoElement.play();
-
-    // Generate thumbnail after a short delay
-    let thumbnailBlob: Blob | null = null;
-    setTimeout(() => {
-      // Create canvas and draw video frame
-      const canvas = document.createElement('canvas');
-      canvas.width = videoElement.videoWidth;
-      canvas.height = videoElement.videoHeight;
-      const ctx = canvas.getContext('2d');
-      ctx?.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-
-      // Convert canvas to blob
-      canvas.toBlob(
-        (blob) => {
-          if (blob) {
-            thumbnailBlob = blob;
-          }
-        },
-        'image/jpeg',
-        0.8
-      );
-    }, 2000); // Capture frame after 1 second
-
-    // Handle recording
     mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
     mediaRecorder.onstop = async () => {
       const videoBlob = new Blob(chunks, { type: 'video/webm' });
+      // Create video element to capture thumbnail
+      const videoElement = document.createElement('video');
+      const videoUrl = URL.createObjectURL(videoBlob);
+      videoElement.src = videoUrl;
+      await this.waitForDataToBeLoaded(videoElement);
+      let thumbnailBlob = await this.createThumbnail(videoElement);
+      if (thumbnailBlob) {
+        const id = getRandomUUID();
+        const duration = videoElement.duration;
+        const createdAt = new Date();
+        this.store.dispatch(
+          new SaveVideo({
+            id,
+            duration,
+            createdAt,
+            thumbnail: thumbnailBlob,
+            video: videoBlob,
+          })
+        );
 
-      // Wait until thumbnail is generated
-      const waitForThumbnail = async () => {
-        if (thumbnailBlob) {
-          const id = getRandomUUID();
-          const duration = videoElement.duration;
-          const createdAt = new Date();
-
-          this.store.dispatch(
-            new SaveVideo({
-              id,
-              duration,
-              createdAt,
-              thumbnail: thumbnailBlob,
-              video: videoBlob,
-            })
-          );
-
-          // Clean up
-          videoElement.pause();
-          videoElement.srcObject = null;
-        } else {
-          // Check again in 100ms
-          setTimeout(waitForThumbnail, 100);
-        }
-      };
-
-      waitForThumbnail();
+        // Clean up
+        videoElement.pause();
+        videoElement.src = '';
+        URL.revokeObjectURL(videoUrl);
+      }
     };
 
     mediaRecorder.start();
     setTimeout(() => mediaRecorder.stop(), this.#config.duration);
   }
+
+  //TODO: decrease the size of the thumbnail
+  createThumbnail(videoElement: HTMLVideoElement): Promise<Blob | null> {
+    return new Promise((resolve) => {
+      videoElement.currentTime = Math.random() * videoElement.duration;
+
+      videoElement.addEventListener('seeked', () => {
+        if (videoElement.readyState >= 2) {
+          const canvas = document.createElement('canvas');
+          canvas.width = videoElement.videoWidth;
+          canvas.height = videoElement.videoHeight;
+          const ctx = canvas.getContext('2d');
+          ctx?.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+          canvas.toBlob(
+            (blob) => {
+              if (blob) {
+                resolve(blob);
+                videoElement.removeEventListener('seeked', () => {});
+              }
+            },
+            'image/jpeg',
+            1
+          );
+        }
+      });
+    });
+  }
+
+  waitForDataToBeLoaded(videoElement: HTMLVideoElement) {
+    return new Promise((resolve, reject) => {
+      videoElement.addEventListener('loadedmetadata', () => {
+        // Chrome bug: https://bugs.chromium.org/p/chromium/issues/detail?id=642012
+        if (videoElement.duration === Infinity) {
+          videoElement.currentTime = Number.MAX_SAFE_INTEGER;
+          videoElement.ontimeupdate = () => {
+            videoElement.ontimeupdate = null;
+            resolve(videoElement.duration);
+            videoElement.currentTime = 0;
+          };
+        }
+        // Normal behavior
+        else resolve(videoElement.duration);
+
+        videoElement.removeEventListener('loadedmetadata', () => {});
+      });
+      videoElement.onerror = (event) => reject(event);
+    });
+  }
+
+  //TODO: cleanup
 }
